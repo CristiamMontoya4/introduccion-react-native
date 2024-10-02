be9ff1c028f22ba0f18fcdd548e970bea983ffeb
CristiamMontoya4 <cristiam.montoya.avam@posbrava.io>
2024-10-02 18:12:18 -0500

    Se crea una navegacion usando una barra lateral

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import NavigationStack from './src/navigation/NavigationStack';
 import NavigationTab from './src/navigation/NavigationTab';
+import NavigationDrawer from './src/navigation/NavigationDrawer';
 
 export default function App() {
 
 
   return (
     <NavigationContainer>
-      {/* <NavigationStack /> */}
-      <NavigationTab />
+      {/* <NavigationStack />  */}  
+      {/* <NavigationTab /> */}
+      <NavigationDrawer />
     </NavigationContainer>
   );
 }
