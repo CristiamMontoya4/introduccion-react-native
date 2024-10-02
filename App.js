7efa9d626ac91f1716ea426c2d92efe296c0e12c
CristiamMontoya4 <cristiam.montoya.avam@posbrava.io>
2024-10-02 15:25:50 -0500

    Se crea carpeta src y componentes, para organizar el proyecto

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
+import LoginForm from './src/components/LoginFom';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <StatusBar style="light" />
       <Image source={{ uri: "https://es.mypet.com/wp-content/uploads/sites/23/2021/03/GettyImages-623368750-e1582816063521-1.jpg" }} style={{
-          width: 100, 
-          height: 100,
+          width: 200,   //el tamaño es obligatorio, cuando se trae de internet
+          height: 200,
       }} />
-      <Text>Open up App.js to start working on your app! 😺💸</Text>
-      <StatusBar style="auto" />
+      <Text style={{ color: 'white' }}>Open up App.js to start working on your app! 😺💸</Text>
+      <LoginForm />
     </View>
   );
 }
@@ -17,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ddd',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/components/LoginFom.js
@@ -0,0 +1,10 @@
+import {TextInput, Button, Text, View} from "react-native"
+
+export default function LoginForm() {
+
+    return(
+        <View>
+            <Text style={{color: 'red' }}>Login</Text>
+        </View>
+    );
+}
