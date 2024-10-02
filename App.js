06e52c9e80a4ab5fa0af85c652434f26be15f3bb
CristiamMontoya4 <cristiam.montoya.avam@posbrava.io>
2024-10-02 16:13:55 -0500

    importaciones para usar navigation

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import LoginForm from './src/components/LoginFom';
